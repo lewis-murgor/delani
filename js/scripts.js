c11895a9c75c9c4f69e3244e10b6d646b9ae940a
Lewis <lewis.murgor@student.moringaschool.com>
2022-06-06 18:22:03 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,53 @@ $(document).ready(function() {
         alert("We have received your message " + user + ". " + "Thankyou for reaching out to us.");
     });
 });
+
+$(document).ready(function() {
+    $(".work1").mouseover(function() {
+        $("#delani").show();
+    }).mouseout(function() {
+        $("#delani").hide();
+    });
+
+    $(".work2").mouseover(function() {
+        $("#ontario").show();
+    }).mouseout(function() {
+        $("#ontario").hide();
+    });
+
+    $(".work3").mouseover(function() {
+        $("#delani-studio").show();
+    }).mouseout(function() {
+        $("#delani-studio").hide();
+    });
+
+    $(".work4").mouseover(function() {
+        $("#weather").show();
+    }).mouseout(function() {
+        $("#weather").hide();
+    });
+
+    $(".work5").mouseover(function() {
+        $("#day").show();
+    }).mouseout(function() {
+        $("#day").hide();
+    });
+
+    $(".work6").mouseover(function() {
+        $("#age").show();
+    }).mouseout(function() {
+        $("#age").hide();
+    });
+
+    $(".work7").mouseover(function() {
+        $("#burner").show();
+    }).mouseout(function() {
+        $("#burner").hide();
+    });
+
+    $(".work8").mouseover(function() {
+        $("#giraffe").show();
+    }).mouseout(function() {
+        $("#giraffe").hide();
+    });
+});
